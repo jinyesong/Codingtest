67cdfd0d202b55a3a220b901d7465af9466ed5bb
Yejin Song <songyejin2000@gmail.com>
2023-11-14 16:36:22 +0900

    Update Solution.java

--- SW_Expert_Academy/D2/Solution.java
+++ SW_Expert_Academy/D2/Solution.java
@@ -3,12 +3,13 @@ package D2;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
-
-        for (int test_case = 1; test_case <= T; test_case++) {
-        }
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			
+		}
     }
 }
