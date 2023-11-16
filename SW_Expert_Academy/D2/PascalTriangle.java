a30673ce2796128aeae8b697f1055d81e3ff7025
Yejin Song <songyejin2000@gmail.com>
2023-11-16 22:20:11 +0900

    add D2 1 solution

--- SW_Expert_Academy/D2/PascalTriangle.java
+++ SW_Expert_Academy/D2/PascalTriangle.java
@@ -1,5 +1,41 @@
 package D2;
 
-public class PascalTriangle {
+import java.util.Scanner;
 
+public class PascalTriangle {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++){
+			int input = sc.nextInt();
+			int[][] pascalTriangle = new int[input][];
+			
+			for(int i=0; i<input; i++) {
+				int[] newArr = new int[i+1];
+				if(i<=1) {
+					for(int j=0; j<i+1; j++) {
+						newArr[j] = 1;
+					}
+				}
+				else { //i°¡ 1ÀÌ»ó
+					newArr[0] = 1;
+					int[] prevArr = pascalTriangle[i-1];
+					for(int j=1; j<i; j++) {
+						newArr[j] = prevArr[j-1] + prevArr[j];
+					}
+					newArr[i] = 1;
+				}
+				pascalTriangle[i] = newArr;
+			}
+			
+			System.out.println("#"+test_case);
+			for(int i=0; i<input; i++) {
+				for(int j=0; j<i+1; j++) {
+					System.out.print(pascalTriangle[i][j] + " ");
+				}
+				System.out.println();
+			}	
+		}
+    }
 }
